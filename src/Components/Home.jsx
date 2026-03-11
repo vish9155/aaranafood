605a3765b1aebdc5a516013de9115ad1ecb048a9
vishal kumar <vishaljpkumar4970@gmail.com>
2026-03-11 17:39:49 +0530

    new2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router'
-import {  FaEnvelope, FaWhatsapp } from 'react-icons/fa'
+import { FaEnvelope, FaWhatsapp } from 'react-icons/fa'
 import { Mail, MapPin, PhoneCall } from 'lucide-react'
 
 export default function Home() {
@@ -20,23 +20,23 @@ export default function Home() {
 
                 <div className="relative z-10 flex items-center justify-center h-full px-4 text-center">
                     <h1 className="text-white font-bold text-xl sm:text-2xl md:text-4xl lg:text-5xl max-w-4xl leading-snug">
-                      Premium Bakery Ingredients Supplier – Aaranafood
+                        Premium Bakery Ingredients Supplier – Aaranafood
                     </h1>
                 </div>
 
             </section>
             <section data-aos='zoom-in-left'>
                 <div className='max-w-5xl px-3 mx-auto py-10 animated-bounce'>
-                     <p className='text-center text-sm sm:text-base text-gray-600 p-1 sm:p-2 md:p-3'>
-                      Aaranafood is a trusted supplier of premium bakery ingredients designed for professional bakeries, food manufacturers, restaurants, and dessert brands. We provide high-quality cocoa powder, fruit paste, fruit based toppings, pizza flour, sausage, and other essential bakery ingredients that help businesses maintain consistent taste, texture, and product quality.
+                    <p className='text-center text-sm sm:text-base text-gray-600 p-1 sm:p-2 md:p-3'>
+                        Aaranafood is a trusted supplier of premium bakery ingredients designed for professional bakeries, food manufacturers, restaurants, and dessert brands. We provide high-quality cocoa powder, fruit paste, fruit based toppings, pizza flour, sausage, and other essential bakery ingredients that help businesses maintain consistent taste, texture, and product quality.
                     </p>
-                     <p className='text-center text-sm sm:text-base text-gray-600 p-1 sm:p-2 md:p-3'>
-                       With a strong focus on quality and reliability, Aaranafood ensures that every ingredient meets industry standards and supports commercial food production. Our products are widely used by bakeries, cafés, pizza outlets, and food processing businesses looking for dependable baking ingredients.
-                     </p>
+                    <p className='text-center text-sm sm:text-base text-gray-600 p-1 sm:p-2 md:p-3'>
+                        With a strong focus on quality and reliability, Aaranafood ensures that every ingredient meets industry standards and supports commercial food production. Our products are widely used by bakeries, cafés, pizza outlets, and food processing businesses looking for dependable baking ingredients.
+                    </p>
 
-                 </div>                
+                </div>
             </section>
-             <section data-aos="zoom-in-right">
+            <section data-aos="zoom-in-right">
                 <div className='max-w-7xl mx-auto px-3 py-10 grid grid-cols-1 md:grid-cols-2 gap-8'>
                     <div className="order-2 md:order-1 space-y-4">
 
@@ -49,7 +49,7 @@ export default function Home() {
                                 AI Food is a next-generation food platform that blends technology with taste. Using the power of artificial intelligence, we analyze preferences, dietary needs, and trends to deliver smarter, healthier, and more personalized food experiences.
                             </p>
                             <p className='text-justify text-sm sm:text-base text-gray-600 p-1 sm:p-2 md:p-3'>
-                               Our system understands what you love to eat and suggests meals that match your lifestyle. Whether you're looking for healthy options, quick bites, or customized meal plans, AI Food ensures every recommendation feels tailor-made for you.
+                                Our system understands what you love to eat and suggests meals that match your lifestyle. Whether you're looking for healthy options, quick bites, or customized meal plans, AI Food ensures every recommendation feels tailor-made for you.
                             </p>
                         </div>
 
@@ -61,46 +61,161 @@ export default function Home() {
                     </div>
                 </div>
             </section>
-<section>
-    <div className='max-w-6xl mx-auto my-10 px-3'>
-    <h2 className="text-center text-xl sm:text-2xl md:text-3xl py-3 ">Our Product Range</h2>
-    <div className="p-2 sm:p-3 md:p-4">
-       <p className='text-center text-sm sm:text-base text-gray-600  max-w-3xl mx-auto'>Aaranafood offers a carefully selected range of bakery ingredients that are widely used in baking, confectionery production, dessert preparation, and commercial food manufacturing.</p>
-    </div>
-    <div className="p-2 sm:p-3 md:p-4">
-        <div className='mx-auto grid grid-cols-1 md:grid-cols-3 gap-10'>
-           <div className='overflow-hidden relative group rounded-2xl'>
-            <img src="" className='h-[220px] w-[320px] group-hover:scale-110' alt="" />      
-             <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Cocoa Powder (Natural & Alkalized)</h2>        
-           </div>
-           <div className='overflow-hidden relative group rounded-2xl'>
-            <img src="" className='h-[220px] w-[320px] group-hover:scale-110' alt="" />      
-             <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Fruit Paste</h2>        
-           </div>
-            <div className='overflow-hidden relative group rounded-2xl'>
-            <img src="" className='h-[220px] w-[320px] group-hover:scale-110' alt="" />      
-             <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Fruit Based Toppings</h2>        
-           </div>
-            <div className='overflow-hidden relative group rounded-2xl'>
-            <img src="" className='h-[220px] w-[320px] group-hover:scale-110' alt="" />      
-             <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Dessert Toppings</h2>        
-           </div>
-            <div className='overflow-hidden relative group rounded-2xl'>
-            <img src="" className='h-[220px] w-[320px] group-hover:scale-110' alt="" />      
-             <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Pizza Flour</h2>        
-           </div>
-            <div className='overflow-hidden relative group rounded-2xl'>
-            <img src="" className='h-[220px] w-[320px] group-hover:scale-110' alt="" />      
-             <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Sausage</h2>        
-           </div>
-          
-        </div>
-         <p  className='text-center text-sm sm:text-base text-gray-600  max-w-xl mx-auto py-10'>Each product is designed to deliver excellent flavor, consistency, and performance in professional kitchens and bakeries.</p>
-    </div>
-    </div>
-</section>
+            <section>
+                <div className='max-w-6xl mx-auto my-10 px-3'>
+                    <h2 className="text-center text-xl sm:text-2xl md:text-3xl py-3 ">Our Product Range</h2>
+                    <div className="p-2 sm:p-3 md:p-4">
+                        <p className='text-center text-sm sm:text-base text-gray-600  max-w-3xl mx-auto'>Aaranafood offers a carefully selected range of bakery ingredients that are widely used in baking, confectionery production, dessert preparation, and commercial food manufacturing.</p>
+                    </div>
+                    <div className="p-2 sm:p-3 md:p-4">
+                        <div className='mx-auto grid grid-cols-1 md:grid-cols-3 gap-10'>
+                            <div className='overflow-hidden relative group rounded-2xl'>
+                                <img src="/images/Fruit-paste.jpeg" className='h-48 object-cover w-full group-hover:scale-110' alt="" />
+                                <div><h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Cocoa Powder (Natural & Alkalized)</h2></div>
+                            </div>
+                            <div className='overflow-hidden relative group rounded-2xl'>
+                                <img src="/images/Fruit-paste.jpeg" className='h-48 object-cover w-full group-hover:scale-110' alt="" />
+                                <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Fruit Paste</h2>
+                            </div>
+                            <div className='overflow-hidden relative group rounded-2xl'>
+                                <img src="/images/Fruits-based-topping1.jpeg" className='h-48 object-cover w-full group-hover:scale-110' alt="" />
+                                <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Fruit Based Toppings</h2>
+                            </div>
+                            <div className='overflow-hidden relative group rounded-2xl'>
+                                <img src="/images/Dessert-Toppings.jpeg" className='h-48 object-cover w-full group-hover:scale-110' alt="" />
+                                <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Dessert Toppings</h2>
+                            </div>
+                            <div className='overflow-hidden relative group rounded-2xl'>
+                                <img src="/images/Pizza-Flour.jpeg" className='h-48 object-cover w-full group-hover:scale-110' alt="" />
+                                <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Pizza Flour</h2>
+                            </div>
+                            <div className='overflow-hidden relative group rounded-2xl'>
+                                <img src="/images/Sausage1.jpeg" className='h-48 object-cover w-full group-hover:scale-110' alt="" />
+                                <h2 className="text-center text-lg sm:text-xl md:text-2xl py-3 ">Sausage</h2>
+                            </div>
 
-             <section className='max-w-4xl py-12 mx-auto'>
+                        </div>
+                        <p className='text-center text-sm sm:text-base text-gray-600  max-w-xl mx-auto py-10'>Each product is designed to deliver excellent flavor, consistency, and performance in professional kitchens and bakeries.</p>
+
+                        <div className='p-2 sm:p-3 md:p-4'>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-8">
+
+
+
+                                <div className="bg-white rounded-2xl border border-white shadow-md hover:shadow-xl transition duration-300 overflow-hidden group">
+
+                                    <div className="overflow-hidden">
+                                        <img
+                                            src={'/images/Pizza-Flour-for-Professional-Kitchens.jpeg'}
+                                            alt={''}
+                                            className="w-full h-48 object-cover group-hover:scale-110 transition duration-300"
+                                        />
+                                    </div>
+
+                                    <div className="p-5">
+
+                                        <h3 className="text-lg font-semibold mb-2 ">
+                                            Pizza Flour for Professional Kitchens
+                                        </h3>
+
+                                        <p className="text-gray-600 text-sm mb-4 text-justify">
+                                            Our pizza flour is specially formulated to deliver the perfect dough texture required for authentic pizzas. Aaranafood pizza flour ensures excellent elasticity, strong gluten structure, and consistent baking performance
+                                        </p>
+
+
+
+                                    </div>
+
+                                </div>
+                                <div className="bg-white rounded-2xl border border-white shadow-md hover:shadow-xl transition duration-300 overflow-hidden group">
+
+                                    <div className="overflow-hidden">
+                                        <img
+                                            src={'/images/Cocoa-Powder-Supplier.jpeg'}
+                                            alt={''}
+                                            className="w-[340px] h-[190px] object-cover group-hover:scale-110 transition duration-300"
+                                        />
+                                    </div>
+
+                                    <div className="p-5">
+
+                                        <h3 className="text-lg font-semibold mb-2 ">
+                                            Cocoa Powder Supplier
+                                        </h3>
+
+                                        <p className="text-gray-600 text-sm mb-4 text-justify">
+                                            Aaranafood supplies premium natural and alkalized cocoa powder used in cakes, brownies, chocolate desserts, and bakery products. Our cocoa powder provides rich flavor, deep color, and smooth texture, making it ideal for professional baking and confectionery production.
+                                        </p>
+
+
+
+                                    </div>
+
+                                </div>
+                                <div className="bg-white rounded-2xl border border-white shadow-md hover:shadow-xl transition duration-300 overflow-hidden group">
+
+                                    <div className="overflow-hidden">
+                                        <img
+                                            src={'/images/'}
+                                            alt={''}
+                                            className="w-full h-48 object-cover group-hover:scale-110 transition duration-300"
+                                        />
+                                    </div>
+
+                                    <div className="p-5">
+
+                                        <h3 className="text-lg font-semibold mb-2 ">
+                                            Fruit Toppings and Fruit Paste
+                                        </h3>
+
+                                        <p className="text-gray-600 text-sm mb-4 text-justify">
+                                            Fruit based toppings and fruit paste from Aaranafood are widely used in cakes, pastries, ice creams, and desserts. These ingredients add vibrant flavor, appealing color, and smooth consistency, helping bakeries and dessert brands create high-quality products
+                                        </p>
+
+
+
+                                    </div>
+
+                                </div>
+
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </section>
+            <section className='max-w-4xl py-12 mx-auto'>
+                <div data-aos="flip-down" className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+                  <div className='order-1 md:order-1'>
+                    <h2 className='text-center text-lg sm:text-xl md:text-2xl py-3 '>Why Choose Aaranafood</h2>
+                    <ul className='p-3 sm:p-4 md:p-5 list-disc list-inside pl-2'>
+                        <li>High Quality Bakery Ingredients</li>
+                        <li>Consistent Taste and Performance</li>
+                        <li>Suitable for Commercial Baking</li>
+                        <li>Trusted by Food Businesses</li>
+                        <li>Reliable Ingredient Supply</li>
+                        
+                    </ul>
+                     <p className='text-center text-sm sm:text-base text-gray-600 p-1 sm:p-2 md:p-3'>Aaranafood focuses on providing dependable ingredients that help bakeries and food manufacturers maintain consistent product quality.</p>
+                  </div>
+                  <div className='order-2 md:order-2'>
+                    <h2 className='text-center text-lg sm:text-xl md:text-2xl py-3 '>Industries We Serve</h2>
+                   
+                      <ul className='p-3 sm:p-4 md:p-5 list-disc list-inside pl-2'>
+                        <li>Professional Bakeries</li>
+                        <li>Cake Manufacturers</li>
+                        <li>Restaurants and Cafés</li>
+                        <li>Pizza Outlets</li>
+                        <li>Food Processing Companies</li>
+                        <li>Dessert Brands</li>
+                      </ul>
+                        <p className='text-center text-sm sm:text-base text-gray-600 p-1 sm:p-2 md:p-3'>Aaranafood supplies bakery ingredients to a wide range of businesses including</p>
+                  </div>
+                </div>
+            </section>
+            <section className='max-w-4xl py-12 mx-auto'>
                 <div data-aos="flip-up">
                     <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-15 p-5'>
 
@@ -148,11 +263,11 @@ export default function Home() {
                 </div>
 
             </section>
-               <section >
+            <section >
                 <div className="max-w-5xl mx-auto py-12 px-4 bg-blue-100 ">
                     <h2 className='text-center text-lg sm:text-xl md:text-2xl'>We’re Here for Your Premium Travel Needs</h2>
                     <p className='max-w-5xl mx-auto p-2 sm:p-3 md:p-4 text-gray-600 text-sm sm:text-base text-center'>
-                      We're here to help with any inquiries you have about booking your next first-class or business-class flight as well as answering any questions about quotes or providing help with existing reservations. With a first-class flight, you will experience exceptional customer service from initial contact until your flight is over with zero difficulties.
+                        We're here to help with any inquiries you have about booking your next first-class or business-class flight as well as answering any questions about quotes or providing help with existing reservations. With a first-class flight, you will experience exceptional customer service from initial contact until your flight is over with zero difficulties.
                     </p>
                 </div>
                 <div className="max-w-5xl mx-auto py-12 px-4 p-3 sm:p-4 md:p-5 grid grid-cols-1 md:grid-cols-2 gap-7">
@@ -186,7 +301,7 @@ export default function Home() {
                                             to="mailto:support@gmail.com"
                                             className="text-blue-600 hover:text-red-600  hover:underline transform hover:-translate-x-2 hover:decoration-2 hover:decoration-blue-800 transition-all duration-300 ease-in"
                                         >
-                                           info@firstclassflight.com
+                                            info@firstclassflight.com
                                         </NavLink>
                                     </p>
                                 </div>
